a3429c7f9e24bd1278b70bedb32c24867ba7e40e
Tengku Aideed Amsyar <82840182+aideedamsyar@users.noreply.github.com>
2024-09-07 18:09:53 +0900

    Update navbar.tsx

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -22,37 +22,31 @@ const Navbar = () => {
             <ul
                 tabIndex={0}
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                <li><a>Item 1</a></li>
+                <li><a>Profile</a></li>
                 <li>
-                <a>Parent</a>
-                <ul className="p-2">
+                <a>Contacts</a>
+                {/* <ul className="p-2">
                     <li><a>Submenu 1</a></li>
                     <li><a>Submenu 2</a></li>
-                </ul>
+                </ul> */}
                 </li>
-                <li><a>Item 3</a></li>
+                <li><a>Chat</a></li>
             </ul>
             </div>
             <a className="btn btn-ghost text-xl">WellChecked</a>
         </div>
         <div className="navbar-center hidden lg:flex">
             <ul className="menu menu-horizontal px-1">
-            <li><a>Item 1</a></li>
-            <li>
-                <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                    <li><a>Submenu 1</a></li>
-                    <li><a>Submenu 2</a></li>
-                </ul>
-                </details>
-            </li>
-            <li><a>Item 3</a></li>
+            <li><a>Profile</a></li>
+            <li><a>Contact</a></li>
+            <li><a>Chat</a></li>
             </ul>
         </div>
         <div className="navbar-end">
-            <a className="btn">Button</a>
+            <a className="btn mr-2">Login</a>
+            <a className="btn">Logout</a>
         </div>
+    
         </div>
   )
 }
