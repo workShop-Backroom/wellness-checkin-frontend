50dd5a0fa29ee11c0e329392b994b2408f2f264d
Tengku Aideed Amsyar <82840182+aideedamsyar@users.noreply.github.com>
2024-09-07 17:08:07 +0900

    added styling

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -22,7 +22,7 @@ const contacts = () => {
       <Navbar />
       <div className="container mx-auto px-4">
         <h1 className="text-3xl font-bold text-center my-6">Contacts</h1>
-        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
           {contacts.map((contact) => (
             <ContactsCard key={contact.id} contacts={contact}/>
           ))}
--- components/contacts/contacts-card.tsx
+++ components/contacts/contacts-card.tsx
@@ -4,21 +4,20 @@ interface CardProps {
   contacts: contacts
 }
 
-const ContactsCard: React.FC<CardProps> = ({ contacts }: CardProps) => {
-
-
+const ContactsCard: React.FC<CardProps> = ({ contacts }) => {
   return (
-    <div className="card bg-base-100 w-96 shadow-xl">
-      <figure>
+    <div className="card bg-base-100 shadow-xl">
+      <figure className="p-4">
         <img 
           src={contacts.image} 
-          alt="contact" 
+          alt="contact"
+          className="w-full h-48 object-cover md:h-56 lg:h-64 rounded-lg" // Responsive height
         />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{contacts.name}</h2>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Call</button>
+          <button className="btn btn-primary">Buy Now</button>
         </div>
       </div>
     </div>
